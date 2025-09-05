d073fd112b70869d807bc0bdc730164800a9d891
Rafal Niestrzeba <beast.art.rn@gmail.com>
2025-09-05 18:46:23 +0200

    Obsługa losowania ruchu komputera (kamień/papier/nożyce)

--- js/script.js
+++ js/script.js
@@ -8,7 +8,29 @@ function clearMessages(){
     document.getElementById('messages').innerHTML = '';
 }
 
-let computerMove = 'kamień';
+clearMessages();
+
+// Losowanie liczby 1–3 (oraz komunikaty szkoleniowe jak w przykładzie)
+let randomFraction = Math.random();
+printMessage('Wylosowany ułamek to: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
+
+// Rozpoznanie ruchu komputera na podstawie wylosowanej liczby
+let computerMove;
+if (roundNumber === 1) {
+    computerMove = 'kamień';
+} else if (roundNumber === 2) {
+    computerMove = 'papier';
+} else if (roundNumber === 3) {
+    computerMove = 'nożyce';
+}
+
+// Komunikaty o ruchach
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
 let playerMove = 'papier';
